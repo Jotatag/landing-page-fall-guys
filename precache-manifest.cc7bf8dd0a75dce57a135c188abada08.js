c5c9e3da1850ea5c06bb8dab61f257cb1c426783
João Pedro Tagliaboa <joao.tagliaboa@pti.org.br>
2022-11-18 18:11:38 -0300

    Updates

---
+++ precache-manifest.cc7bf8dd0a75dce57a135c188abada08.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "7677648d69231352e3ec",
+    "url": "/landing-page-fall-guys/static/css/main.135acf04.chunk.css"
+  },
+  {
+    "revision": "7677648d69231352e3ec",
+    "url": "/landing-page-fall-guys/static/js/main.7677648d.chunk.js"
+  },
+  {
+    "revision": "5b5e2613c1c51808e1db",
+    "url": "/landing-page-fall-guys/static/js/1.5b5e2613.chunk.js"
+  },
+  {
+    "revision": "eb76eca963c7586c8e17",
+    "url": "/landing-page-fall-guys/static/js/runtime~main.eb76eca9.js"
+  },
+  {
+    "revision": "419c76429aa89ee4fbb512c8f60ac0bc",
+    "url": "/landing-page-fall-guys/static/media/background-header2.419c7642.png"
+  },
+  {
+    "revision": "87dddfd0f2ad40532f81aa57cd8c00fa",
+    "url": "/landing-page-fall-guys/static/media/logo.87dddfd0.png"
+  },
+  {
+    "revision": "a43e6e5e45c9d225308d3b01f89846b7",
+    "url": "/landing-page-fall-guys/static/media/mascot.a43e6e5e.png"
+  },
+  {
+    "revision": "6520c99910555533320110482ef2ca94",
+    "url": "/landing-page-fall-guys/static/media/carousel1.6520c999.png"
+  },
+  {
+    "revision": "6122d80bd11182168b49adb26d6108fa",
+    "url": "/landing-page-fall-guys/static/media/carousel2.6122d80b.png"
+  },
+  {
+    "revision": "1300cd9bf40abd5a94dd93e7c6b1cb9b",
+    "url": "/landing-page-fall-guys/static/media/carousel3.1300cd9b.png"
+  },
+  {
+    "revision": "a15694aebe61fe4a7dbe2a2fe58a0d1b",
+    "url": "/landing-page-fall-guys/static/media/carousel4.a15694ae.png"
+  },
+  {
+    "revision": "20b525d324b06242d3ef1616329f5fdc",
+    "url": "/landing-page-fall-guys/static/media/background-buy-section.20b525d3.png"
+  },
+  {
+    "revision": "175069c3d71223c4dadc37548cb80541",
+    "url": "/landing-page-fall-guys/static/media/mascot2.175069c3.png"
+  },
+  {
+    "revision": "d45ed222d6216f9bfea1cf670dd3ef42",
+    "url": "/landing-page-fall-guys/static/media/carousel_pagination.d45ed222.svg"
+  },
+  {
+    "revision": "e36c928082f3e49bab3296d0bbbc2344",
+    "url": "/landing-page-fall-guys/static/media/arrow_up.e36c9280.svg"
+  },
+  {
+    "revision": "05bacdb5f46b8cd2c6bd7a0409a4e4d9",
+    "url": "/landing-page-fall-guys/index.html"
+  }
+];
